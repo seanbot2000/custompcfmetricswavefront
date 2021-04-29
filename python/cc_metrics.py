8c59b7f641fbe27e54a6c61b340ea557343329c7
Sean Noyes <sean@razor>
2021-04-29 10:38:21 -0500

    added services to python script; cleaned up metric point tags

--- python/cc_metrics.py
+++ python/cc_metrics.py
@@ -7,25 +7,27 @@ import time
 
 debug = True
 
-cloud_controller     = 'https://CHANGEME/'
+cloud_controller     = 'https://api.sys.xxx.xxx'
 deploy_client_id     = 'cf'
 deploy_client_secret = ''
 verify_ssl           = False
 
-username = 'CHANGEME'
-Password = 'CHANGEME'
+username = ''
+Password = ''
 
-proxy_host = 'CHANGEME'
+proxy_host = 'localhost'
 proxy_port = '2878'
 
-AppsTotalName   = 'pcf.prime.TotalApps '
-ServicesTotalName = 'pcf.prime.TotalServices'
-SpacesName      = 'pcf.prime.Spaces'
-OrgName         = 'pcf.prime.Orgs'
-AppsName        = 'pcf.prime.Apps'
-ServicesName    = 'pcf.prime.Services'
-OrgTotalName    = 'pcf.prime.TotalOrgs '
-SpacesTotalName = 'pcf.prime.TotalSpaces '
+metrics_pre = ''
+
+AppsTotalName   =  metrics_pre + '.TotalApps'
+ServicesTotalName = metrics_pre + '.TotalServices'
+SpacesName      = metrics_pre + '.Spaces'
+OrgName         = metrics_pre + '.Orgs'
+AppsName        = metrics_pre + '.Apps'
+ServicesName    = metrics_pre + '.Services'
+OrgTotalName    = metrics_pre + '.TotalOrgs '
+SpacesTotalName = metrics_pre + '.TotalSpaces '
 
 cc = cf_api.new_cloud_controller(
     cloud_controller,
@@ -33,7 +35,8 @@ cc = cf_api.new_cloud_controller(
     client_secret=deploy_client_secret,
     username=username,
     password=Password,
-).set_verify_ssl(verify_ssl)
+    verify_ssl=verify_ssl
+)
 
 def sendMetric(metric):
     entireMetric = metric + '| nc -q0 ' + proxy_host + ' ' + proxy_port
@@ -47,7 +50,7 @@ res = req.get()
 orgs = res.resources
 orgCounter = 0
 for r in orgs:
-    OrgTags = ' OrgName=' + r.name + ',OrgGUID="' + r.guid + '" '
+    OrgTags = ' OrgGUID="' + r.guid + '" OrgName=' + r.name + ' '
     orgCounter +=1
     sendMetric('echo ' + OrgName + ' ' + str(1) + ' source=' + r.name + OrgTags)
     
@@ -59,27 +62,24 @@ spaces = res.resources
 SpacesCounter = 0
 for r in spaces:
     OrgGUID = r.organization_guid
-    SpacesTags = ' SpacesName=' + r.name + ',SpacesGUID="' + r.guid + '",OrgGUID="' + OrgGUID + '" '
+    SpacesTags = ' SpacesName=' + r.name + ' SpacesGUID="' + r.guid + '" OrgGUID="' + OrgGUID + '" '
     SpacesCounter +=1
     sendMetric('echo ' + SpacesName + ' ' + str(1) + ' source=' + r.name + SpacesTags)
     
 sendMetric('echo ' + SpacesTotalName + ' ' + str(SpacesCounter) + ' source=CF')
 
-# List all applications per org
-res = cc.organizations().get()
-orgs = res.resources
-for o in orgs:
-    res = cc.apps().get()
-    apps = res.resources
-    AppsCounter = 0
-    for r in apps:
-        si = r.service_bindings_url
-        si = si.replace('/v2/apps/','')
-        si = si.replace('/service_bindings','')
-        AppState = r.state
-        AppsTags = ' AppsName=' + r.name + ',AppsGUID="' + r.guid + '",siGUID="' + si + '",state=' + AppState + ',org=' + o.name + ' '
-        AppsCounter +=1
-        sendMetric('echo ' + AppsName + ' ' + str(1) + ' source=' + r.name + AppsTags)
+# List applications
+res = cc.apps().get()
+apps = res.resources
+AppsCounter = 0
+for r in apps:
+    si = r.service_bindings_url
+    si = si.replace('/v2/apps/','')
+    si = si.replace('/service_bindings','')
+    AppState = r.state
+    AppsTags = ' AppsName=' + r.name + ' AppsGUID="' + r.guid + '" siGUID="' + si + '" state="' + AppState + '" '
+    AppsCounter +=1
+    sendMetric('echo ' + AppsName + ' ' + str(1) + ' source=' + r.name + AppsTags)
     
 sendMetric('echo ' + AppsTotalName + ' ' + str(AppsCounter) + ' source=CF')
 
@@ -89,10 +89,9 @@ services = res.resources
 ServicesCounter = 0
 for r in services:
     si = r.service_bindings_url
-    si = si.replace('/v2/apps/','')
     si = si.replace('/service_bindings','')
-    ServiceState = r.state
-    ServiceTags = ' ServiceName=' + r.name + ', ServicesGUID="' + si + '",state=' + ServiceState + ' '
+    si = si.replace('/v2/service_instances/','')
+    ServiceTags = ' ServiceName=' + r.name + ' ServicesGUID="' + si + '" '
     ServicesCounter +=1
     sendMetric('echo ' + ServicesName + ' ' + str(1) + ' source=' + r.name + ServiceTags)
     
